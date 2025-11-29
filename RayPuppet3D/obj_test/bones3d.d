064103af1d2c2ebc6bfd9e569a73198ca4c74b03
Victor Berdugo <victorberdugo1@gmail.com>
2025-11-29 19:11:54 +0100

    test

--- RayPuppet3D/obj_test/bones3d.d
+++
@@ -1,7 +0,0 @@
-obj_test/bones3d.o: src/bones3d.c inc/bones_core.h inc/raylib.h \
- inc/raymath.h inc/rlgl.h inc/raylib.h
-inc/bones_core.h:
-inc/raylib.h:
-inc/raymath.h:
-inc/rlgl.h:
-inc/raylib.h:
